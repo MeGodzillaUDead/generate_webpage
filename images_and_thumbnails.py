83cfc142408f26686620265a01d743a418559de2
Jonathan Riezman <riezman@gmail.com>
2017-09-05 17:57:22 -0500

    changed 'w' access to 'a' so it doesn't overwrite the file

--- images_and_thumbnails.py
+++ images_and_thumbnails.py
@@ -10,7 +10,7 @@ def images_list():
 	return(imgs)
 
 def write_file(image_list):
-	output_file = open("index.html","w")
+	output_file = open("index.html","a")
 	for img in image_list:
 		output_file.write('<a target="_blank" href="' + img + '">\n')
 		output_file.write('<img src="' + img + '" alt="' + img + '">\n')
